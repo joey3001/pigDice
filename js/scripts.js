5d68199f7bcbb31b39b5dd767e09b7f7190835ae
Joey3001 <josephnero111@gmail.com>
2020-11-04 15:18:32 -0800

    working functions
    Co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,32 @@ function Score(player1Score, player2Score) {
   this.player2Score = player2Score; 
 }
 
-Score.prototype.scoreAddition = function (player, newScore) {
-  if (player === "player1") {
+Score.prototype.scoreAddition = function (currentPlayer, standbyPlayer, newScore, scoreTotal) {
+  if (currentPlayer === "player1") {
     this.player1Score += newScore; 
+    scoreTotal += newScore; 
     if (this.player1Score >= 100) {
     alert("Player 1 wins");
     }
-    return this.player1Score;
+    if (newScore === 1) {
+      this.player1Score = this.player1Score - scoreTotal;
+      scoreTotal = 0;
+      //[currentPlayer, standbyPlayer, scoreTotal] = switchPlayer(currentPlayer, standbyPlayer, scoreTotal);
+    }
+    return scoreTotal;
   }
   else {
     this.player2Score += newScore; 
+    scoreTotal += newScore; 
     if (this.player2Score >= 100) {
       alert("Player 2 wins");
       }
-    return this.player2Score; 
+    if (newScore === 1) {
+      this.player2Score = this.player2Score - scoreTotal;
+      scoreTotal = 0;
+      alert("Switch Turns!");
+    }
+    return scoreTotal;
   }
 }
 
@@ -24,25 +36,28 @@ function randomNumber () {
   return Math.floor(Math.random() * 6) + 1; 
 }
 
-function switchPlayer (p1,p2) {
+function switchPlayer (p1,p2,scoreTotal) {
   [p1 , p2] = [p2, p1];
-  return [p1,p2]; 
+  scoreTotal = 0;
+  return [p1,p2,scoreTotal]; 
 }
 
 let runningScore = new Score(0,0);
 let currentPlayer; 
 let standbyPlayer; 
 [currentPlayer , standbyPlayer] = ["player1", "player2"];
+let scoreTotalTracker = 0; 
+
 $(document).ready(function() {
   $("form#rollDice").submit(function(event) {
     event.preventDefault();
     let randomNumber1 = randomNumber();
-    runningScore.scoreAddition(currentPlayer, randomNumber1);
+    scoreTotalTracker = runningScore.scoreAddition(currentPlayer, standbyPlayer, randomNumber1, scoreTotalTracker);
     console.log(runningScore);
   });
   $("form#switchPlayer").submit(function(event) {
     event.preventDefault();
-    [currentPlayer, standbyPlayer] = switchPlayer(currentPlayer, standbyPlayer);
+    [currentPlayer, standbyPlayer, scoreTotalTracker] = switchPlayer(currentPlayer, standbyPlayer);
   });
 }); 
 
