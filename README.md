75c18cab648e0017f9b2b07fb603628b3930baa7
Joey3001 <josephnero111@gmail.com>
2020-11-04 11:53:00 -0800

    adds two test specs and functionality to swap the player currently rolling the dice / getting points.
    Co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 | Test | Input | Output |
 | :--------------------------- | :------------------------| :---------------- | 
-| score Addition |||
-| Should iteratively add the users new score to their previous score every time its form is submitted | 5 | previous score + 5 |
+| scoreAddition |||
+| Should iteratively add the users new score to their previous score every time scoreAddition() is called | previous score: 20 + new score : 5 | 25 |
+| Should alert users of a winner when a player's score reaches 100 | player1Score = 100 | "Player 1 wins" | 
+
+| randomNumber |||
+| Should generate random number between 1 - 6 when "roll dice" button is clicked.| n/a | "roll dice" = (number 1-6) |
+
+|switchPlayer |||
+| Should switch player receiveing points for "rolling dice" | joe = current player , parker = standby player | parker = current player , joe = standby player |
--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,49 @@ function Score(player1Score, player2Score) {
   this.player2Score = player2Score; 
 }
 
-
 Score.prototype.scoreAddition = function (player, newScore) {
   if (player === "player1") {
     this.player1Score += newScore; 
+    if (this.player1Score >= 100) {
+    alert("Player 1 wins");
+    }
     return this.player1Score;
   }
   else {
     this.player2Score += newScore; 
+    if (this.player2Score >= 100) {
+      alert("Player 2 wins");
+      }
     return this.player2Score; 
   }
 }
 
-let runningScore = new Score(0,0);
+function randomNumber () {
+  return Math.floor(Math.random() * 6) + 1; 
+}
 
+function switchPlayer (p1,p2) {
+  [p1 , p2] = [p2, p1];
+  return [p1,p2]; 
+}
+
+let runningScore = new Score(0,0);
+let currentPlayer; 
+let standbyPlayer; 
+[currentPlayer , standbyPlayer] = ["player1", "player2"];
 $(document).ready(function() {
   $("form#rollDice").submit(function(event) {
     event.preventDefault();
-    runningScore.scoreAddition("player1", 5);
+    let randomNumber1 = randomNumber();
+    runningScore.scoreAddition(currentPlayer, randomNumber1);
     console.log(runningScore);
   });
+  $("form#switchPlayer").submit(function(event) {
+    event.preventDefault();
+    [currentPlayer, standbyPlayer] = switchPlayer(currentPlayer, standbyPlayer);
+  });
 }); 
+
+
+
+
