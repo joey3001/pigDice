566b2d63d680c4b09ba8050fccee42fae5b49904
Joey3001 <josephnero111@gmail.com>
2020-11-05 12:01:01 -0800

    Adds functionality to play the computer if you'd like to
    Co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 | Should iteratively add the users new score, which is the result of their dice roll, to their previous score every time scoreAddition() is called | previous score(20) + new score(5) | 25 |
 | Should alert users of a winner when a player's score reaches 100 | player1Score = 100 | "Player 1 wins" | 
 | Should subtract total accumulated turn score from player's score upon player rolling a 1. | new score = 1 | previoius score(20) + new score(5) = current score(25) , previous score(25) + new score(1) = current score(20) | 
+| total game score - total turn score | (but use the actual variables)
 | Should switch player turns upon dice rolling a 1 | player1 rolls 1 | switch turns to player 2 |
+                                         ===> | newScore = 1 && currentPlayer = player 1 | set currentPlayer = player 2 |
 | randomNumber |||
 | Should generate random number between 1 - 6 when "roll dice" button is clicked.| n/a | "roll dice" = (number 1-6) |
 | switchPlayer |||
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function Score(player1Score, player2Score) {
+function Score (player1Score, player2Score) {
   this.player1Score = player1Score; 
   this.player2Score = player2Score; 
 }
@@ -14,7 +14,7 @@ Score.prototype.scoreAddition = function (currentPlayer, standbyPlayer, newScore
       this.player1Score = this.player1Score - scoreTotal;
       [currentPlayer, standbyPlayer, scoreTotal] = switchPlayer(currentPlayer, standbyPlayer, scoreTotal);
     }
-    return [currentPlayer, standbyPlayer, scoreTotal]
+    return [currentPlayer, standbyPlayer, scoreTotal];
   }
   else {
     this.player2Score += newScore; 
@@ -26,12 +26,8 @@ Score.prototype.scoreAddition = function (currentPlayer, standbyPlayer, newScore
       this.player2Score = this.player2Score - scoreTotal;
       [currentPlayer, standbyPlayer, scoreTotal] = switchPlayer(currentPlayer, standbyPlayer, scoreTotal);
     }
-    return [currentPlayer, standbyPlayer, scoreTotal]
+    return [currentPlayer, standbyPlayer, scoreTotal];
   }
-}
-
-function randomNumber () {
-  return Math.floor(Math.random() * 6) + 1; 
 }
 
 function switchPlayer (p1,p2,scoreTotal) {
@@ -40,23 +36,83 @@ function switchPlayer (p1,p2,scoreTotal) {
   return [p1,p2,scoreTotal]; 
 }
 
+Score.prototype.scoreAdditionIfPlayingComputer = function (newScore, scoreTotal) {
+    $("#userAlert").hide();
+    this.player1Score += newScore; 
+    scoreTotal += newScore; 
+    if (this.player1Score >= 100) {
+    alert("Player 1 wins");
+    }
+    if (newScore === 1) {
+      this.player1Score = this.player1Score - scoreTotal;
+      let roll = randomNumber();
+      let roll1 = randomNumber(); 
+      this.player2Score += roll; 
+      this.player2Score += roll1; 
+      scoreTotal = 0; 
+      $("#userAlert").show();
+      alert("roll 1 is : " + roll);
+      alert("roll 2 is : " + roll1);
+      if (this.player2Score >= 100) {
+        alert("the computer wins");
+      }
+    }
+    return scoreTotal;
+}
+
+Score.prototype.switchPlayerIfPlayingComputer = function () {
+  let roll = randomNumber();
+  let roll1 = randomNumber(); 
+  this.player2Score += roll; 
+  this.player2Score += roll1; 
+  scoreTotal = 0; 
+  $("#userAlert").show();
+  alert("roll 1 is : " + roll);
+  alert("roll 2 is : " + roll1); 
+  if (this.player2Score > 100) {
+    alert("the computer wins");
+  }
+  return scoreTotal;
+}
+
+function randomNumber () {
+  return Math.floor(Math.random() * 6) + 1; 
+}
+
 $(document).ready(function() {
   let runningScore = new Score(0,0);
   let currentPlayer; 
   let standbyPlayer; 
   [currentPlayer , standbyPlayer] = ["player1", "player2"];
   let scoreTotalTracker = 0; 
+  let x = "human"; 
+  $("#computer").click(function (){
+    x="computer";
+  });
   $("form#rollDice").submit(function(event) {
     event.preventDefault();
     let randomNumber1 = randomNumber();
-    [currentPlayer, standbyPlayer, scoreTotalTracker] = runningScore.scoreAddition(currentPlayer, standbyPlayer, randomNumber1, scoreTotalTracker);
-    $(".player1").html(runningScore.player1Score);
-    $(".player2").html(runningScore.player2Score);
-    $(".newScore").html(randomNumber1);
+    if (x === "human") {
+      [currentPlayer, standbyPlayer, scoreTotalTracker] = runningScore.scoreAddition(currentPlayer, standbyPlayer, randomNumber1, scoreTotalTracker);
+      $(".player1").text(runningScore.player1Score);
+      $(".player2").text(runningScore.player2Score);
+      $(".newScore").text(randomNumber1);
+    }
+    else {
+      scoreTotalTracker = runningScore.scoreAdditionIfPlayingComputer(randomNumber1, scoreTotalTracker);
+      $(".player1").text(runningScore.player1Score);
+      $(".player2").text(runningScore.player2Score);
+      $(".newScore").text(randomNumber1);
+    }
   });
   $("form#switchPlayer").submit(function(event) {
     event.preventDefault();
-    [currentPlayer, standbyPlayer, scoreTotalTracker] = switchPlayer(currentPlayer, standbyPlayer);
+    if (x === "human") {
+      [currentPlayer, standbyPlayer, scoreTotalTracker] = switchPlayer(currentPlayer, standbyPlayer);
+    }
+    else {
+      scoreTotalTracker = runningScore.switchPlayerIfPlayingComputer();
+      $(".player2").text(runningScore.player2Score);
+    }
   });
 }); 
-
